8e24ea49396cfd20c876471054a382e6ffd3e7bb
dwwsb <101042938+dwwsb@users.noreply.github.com>
2022-04-04 18:07:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+"""A simple application for a school automation subject"""
 
-
+from flask import Flask
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
+    """Returns a string"""
     return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
+if __name__ == "__main__":
+    app.run(debug=True)
